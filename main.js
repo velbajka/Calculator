16a11ee9f7621ffff26c2d8b5a0ed5dffcc04de8
Arkadiusz Kuboszek <kuboszek.arkadiusz@gmail.com>
2022-02-19 22:12:13 +0100

    Bardzo podstawowa obsługa operatora *

--- main.js
+++ main.js
@@ -1,9 +1,36 @@
+var liczba = ""
 function enterNumber(id){
     button = document.getElementById(id);
     //console.log(button.value);
 
     output = document.getElementById(100);
-    //console.log(output.value);
-           
+    //console.log(output.value);           
     output.value = output.value + button.value;
+
+    if (id != "*"){
+        liczba = liczba + button.value;
+    }
+}
+
+function multiply(){
+    console.log(liczba)
+
+    wynik = liczba;
+    liczba = "";
+    enterNumber("*");
+    rodzaj_operacji = "*" //rodzaj ostatniej operacji
+}
+
+function equals(){
+    console.log(wynik)
+    
+    console.log(liczba)
+    
+
+    if (rodzaj_operacji == "*"){
+        wynik = parseInt(wynik) * parseInt(liczba);
+        output.value = wynik;
+    }
+
+    liczba = "";
 }
