804c8adf7b265eff8f8c44f66c57be725b86e02b
Agnieszka Bajka <velbajka@gmail.com>
2022-03-05 19:42:01 +0100

    Za dużo zmian - obsługa operatorów + - / =, funkkcje czyszczące

--- main.js
+++ main.js
@@ -36,16 +36,37 @@ function calculate(){
     if (op == "*"){
         res =  parseInt(a) * parseInt(b);
     }
-    //TODO    
+    else if (op == "+"){
+        res =  parseInt(a) + parseInt(b);
+    }
+    else if (op == "-"){
+        res =  parseInt(a) - parseInt(b);
+    }
+    else if (op == "÷"){
+        res =  parseInt(a) / parseInt(b);
+    }
+     
 }
 
 // *, +, /, -
 function handleOperatorButton(operator){
-    refreshOutput(operator);
-    op = operator;
-    a = res;
-    b = "";
+    // console.log(a)
+    if (a != ""){
+        refreshOutput(operator);
+        op = operator;
+        a = res;
+        b = "";
+    }
+
 }
+
+
+// function handleOperatorButton(operator){
+//     refreshOutput(operator);
+//     op = operator;
+//     a = res;
+//     b = "";
+// }
 
 
 // function enterNumber(id){
@@ -88,31 +109,99 @@ function handleOperatorButton(operator){
 // }
 
 function clearAll(){
-    a = ""
-    wynik = ""
-    output.value="" // gorny 
-    output1.value=""    //dolny
-
+    // a = ""
+    // wynik = ""
+    // output.value="" // gorny 
+    // output1.value=""    //dolny
+    a = "";
+    b = "";
+    res = "";
+    op = ""; 
+    let output = document.getElementById(100);
+    output.value =''
+    let output1 = "";
+    refreshOutput1();
 }
 
 function clearLastDigit(){
-    a = a.slice(0, -1);
-    output.value = output.value.slice(0, -1)
+    // a = a.slice(0, -1);
+    // output.value = output.value.slice(0, -1);
+    let output = document.getElementById(100);
+    let temporary_res = res
+    if (op == ""){
+        a = a.slice(0, -1); 
+        res = a;
+        output.value = a;       
+    }
+    else{
+        if (b != ""){
+            b = b.slice(0, -1);
+            if (b!=""){
+                calculate()
+                refreshOutput1();
+                output.value = output.value.slice(0,-1);
+            }
+            else{
+                res == temporary_res
+                output.value = output.value.slice(0,-1);
+                refreshOutput1()
+
+            }
+        }
+        else{
+            op == "";
+            output.value = output.value.slice(0,-1);
+
+        }      
+                  
+    }
 
 }
 
 function displayTotalResult(){
-    let output1 = document.getElementById(101)
-    output1.value = wynik
+
+    let output1 = document.getElementById(101);
+    let output = document.getElementById(100);
+    output1.value = res;
+    output.value = res;
+    console.log(output.value)
+    console.log(output1.value)
+    a = res;
+    b = "";
+    op = "";
 }
 
 
 function clearLastNumber(){
-    console.log(output.value.slice(-1)); 
-    while (output.value.slice(-1) != '*') {
-        output.value = output.value.slice(0,-1);
-        console.log(output.value)
-      }
-    a = ""
+    // console.log(output.value.slice(-1)); 
+    // while (output.value.slice(-1) != '*') {
+    //     output.value = output.value.slice(0,-1);
+    //     console.log(output.value)
+    //   }
+    // a = ""
+    let output = document.getElementById(100);
+    if (op == ""){
+        a = ""; 
+        b = "";
+        res = a;
+        output.value = "";   
+        refreshOutput1();    
+    }
+    else{
+        if (b.length != 0){
+            let len = b.length;
+            console.log(len)
+            b = "";
+            res = a;
+            refreshOutput1();
+            output.value = output.value.slice(0,-len);   
+        }
+        else{
+            op == "";
+            output.value = output.value.slice(0,-1);
+
+        }    
+    }
 }
+
 
